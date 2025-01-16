b1d4589dcf90f9bb174b1fb8fd529dc6b6907385
matteof7 <matteofoti741@gmail.com>
2025-01-16 10:28:43 +0100

    functions if else loops5

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
-for i in range(10):
-    for j in range(10):
-        if i < j:
-            if i == 8 and j == 9:
-                print("{:d}{:d}".format(i, j))
-            else:
-                print("{:d}{:d}, ".format(i, j), end="")
+for i in range(0, 9):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{:02d}".format(i * 10 + j))
+        else:
+            print("{:02d}, ".format(i * 10 + j), end="")
