0ef39391a6091a7650eedee3575531df33114d95
matteof7 <matteofoti741@gmail.com>
2025-01-16 10:15:02 +0100

    functions if else loops5

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,3 @@
 for i in range(0, 10):
     for j in range(i + 1, 10):
-        if i == 8 and j == 9:  # Dernière combinaison
-            print(f"{i}{j}")
-        else:
-            print(f"{i}{j}, ", end="")
+     print(f"{i}{j}", end=", " if i != 8 or j != 9 else "\n")
