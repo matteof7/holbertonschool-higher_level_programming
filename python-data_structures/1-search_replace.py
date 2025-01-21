ea93e479b906229f9c88a6ff66876b144934b97d
matteof7 <matteofoti741@gmail.com>
2025-01-21 02:59:46 +0200

    python data

--- python-data_structures/1-search_replace.py
+++ python-data_structures/1-search_replace.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 
-def search_replace(my_list, search, replace):
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
