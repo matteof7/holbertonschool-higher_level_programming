b224dd212f1bbf3f5ec7bc8e7e12fe5ad10f1efe
matteof7 <matteofoti741@gmail.com>
2025-01-27 14:04:16 +0100

    python classes

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+class Square:
+    """A class that defines a square with a private attribute `size`."""
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance with a private attribute `size`.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+class Square:
+    """A class that defines a square with a private attribute `size` and validations."""
+
+    def __init__(self, size=0):
+        """
+        Initializes a Square instance with a private attribute `size`.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If `size` is not an integer.
+            ValueError: If `size` is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
