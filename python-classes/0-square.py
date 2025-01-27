21efac3b20d225702c96266de8821b8740bc40e0
matteof7 <matteofoti741@gmail.com>
2025-01-27 11:32:40 +0100

    python classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-class square:
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+'''
+Module for Class Square
+'''
+
+
+class Square:
+    '''I the class Square'''
+    pass
