cd1843e78819ff03fe994d830625dd12971c8b2a
matteof7 <matteofoti741@gmail.com>
2025-01-27 14:07:55 +0100

    python classes

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
+
+'''
+Module for the Square class
+'''
+
+
 class Square:
-    """A class that defines a square with a private attribute `size`."""
+    '''A simple class representing a square.'''
 
     def __init__(self, size):
-        """
-        Initializes a Square instance with a private attribute `size`.
-
-        Args:
-            size (int): The size of the square.
-        """
+        '''Initializes the square with a given size.'''
         self.__size = size
+
+    def area(self):
+        '''Calculates the area of the square.'''
+        return self.__size ** 2
+
+if __name__ == "__main__":
+    my_square = Square(5)
+    print(f"The area of the square is: {my_square.area()}.")
