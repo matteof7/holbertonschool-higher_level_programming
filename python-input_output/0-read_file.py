cb679364ea2daf9b0014b2d1c5757bc4ad15f25a
matteof7 <matteofoti741@gmail.com>
2025-02-11 11:10:13 +0100

    input

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,8 +3,6 @@
 This module defines a function that reads a text file (UTF8)
 and prints it to stdout.
 """
-
-
 def read_file(filename=""):
     """
     Reads a UTF-8 text file and prints its content to stdout.
