ec028dba7065bee5ece65aeeac752c9c4893bd88
matteof7 <matteofoti741@gmail.com>
2025-02-21 10:41:37 +0100

    api

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request # type: ignore # Importer les modules Flask
+from flask import Flask, jsonify, request 
 
 # Initialiser l'application Flask
 app = Flask(__name__)
